a8c19155589f54e149d1ed1b39007d7aef7f5ee3
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2025-01-09 17:49:50 +0700

    dfbg

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,6 @@ import { initReactI18next } from 'react-i18next';
 import translationEN from './locales/en/translation.json';
 import translationKH from './locales/kh/translation.json';
 
-
 i18n.use(initReactI18next).init({
     resources: {
         en: {
@@ -18,6 +17,13 @@ i18n.use(initReactI18next).init({
     interpolation: {
         escapeValue: false,
     },
+});
+
+// Listen for language change and update class on <body>
+i18n.on('languageChanged', (lng) => {
+    document.documentElement.lang = lng;
+    document.body.classList.remove('khmer-font', 'english-font');
+    document.body.classList.add(lng === 'kh' ? 'khmer-font' : 'english-font');
 });
 
 export default i18n;
