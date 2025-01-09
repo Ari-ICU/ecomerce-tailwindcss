be1207e8ec6008c9ef76cffb9ac8412ceec45c1f
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2025-01-10 06:52:34 +0700

    drbgj

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -5,20 +5,33 @@ import khFlag from '../assets/images/kh.png'; // Path to your Khmer flag image
 
 const LanguageSwitcher = () => {
     const { i18n } = useTranslation();
+    const selectedLanguage = i18n.language;
 
     const changeLanguage = (lang) => {
-        i18n.changeLanguage(lang);
+        if (selectedLanguage !== lang) {
+            i18n.changeLanguage(lang);
+        }
     };
 
     return (
         <div className="space-x-4">
-            <button onClick={() => changeLanguage('en')} className="flex items-center space-x-2">
+            <button
+                onClick={() => changeLanguage('en')}
+                className={`flex items-center p-1 border rounded ${selectedLanguage === 'en' ? 'bg-gray-300 ' : ''}`}
+                aria-pressed={selectedLanguage === 'en'}
+                aria-label="Switch to English"
+            >
                 <img src={enFlag} alt="English" className="w-6 h-4" />
-                <span>EN</span>
+                <span className="sr-only">English</span>
             </button>
-            <button onClick={() => changeLanguage('kh')} className="flex items-center space-x-2">
+            <button
+                onClick={() => changeLanguage('kh')}
+                className={`flex items-center p-1 border rounded ${selectedLanguage === 'kh' ? 'bg-gray-300 text-black' : ''}`}
+                aria-pressed={selectedLanguage === 'kh'}
+                aria-label="Switch to Khmer"
+            >
                 <img src={khFlag} alt="Khmer" className="w-6 h-4" />
-                <span>KH</span>
+                <span className="sr-only">Khmer</span>
             </button>
         </div>
     );
