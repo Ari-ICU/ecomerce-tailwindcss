8260bb52c0bdd84a6ef5e88d80f85f0c274b45be
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2025-01-09 17:35:19 +0700

    srdtg

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -36,11 +36,9 @@ const HeroSection = () => {
                             <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
                                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
                                     <span className="block xl:inline">{t('hero.title')}</span>
-                                    <span className="block text-indigo-600 xl:inline">{t('hero.title')}</span>
+                                    <span className="block text-indigo-600 xl:inline">{t('hero.subtitle')}</span>
                                 </h1>
-                                <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-                                    {t('hero.subtitle')}
-                                </p>
+
                                 <div className="relative flex flex-col sm:flex-row sm:space-x-4">
                                     <Link
                                         to="/"
